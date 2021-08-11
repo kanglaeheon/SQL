ad71041b947754f78f4fd7c1ebaaac3c6c741274
kanglaeheon <kanglaeheon@gmail.com>
2021-08-11 20:04:30 +0900

    미니프로젝트(Oracle) DB

---
+++ phonedb.sql
@@ -0,0 +1,25 @@
+-- TABLE
+DESC PHONE_BOOK;
+
+CREATE TABLE PHONE_BOOK (
+    id NUMBER(10) PRIMARY KEY,
+    name VARCHAR2(10),
+    hp VARCHAR2(20),
+    tel VARCHAR2(20)
+    );
+
+CREATE SEQUENCE seq_phone_book
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 1000
+    MINVALUE 1;
+    
+SELECT * FROM user_sequences;
+
+DROP SEQUENCE seq_phone_book;
+
+COMMIT;
+
+SELECT * FROM PHONE_BOOK;
+
+-- 시퀀스 보는 구문 추가 할 것ㅎ
