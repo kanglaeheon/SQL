aaaada55726957cb89fff0c989e8e02a83a41684
kanglaeheon <kanglaeheon@gmail.com>
2021-10-05 11:40:22 +0900

    webdb.sql 추가

--- WEBDB.sql
+++ WEBDB.sql
@@ -45,3 +45,38 @@ CREATE TABLE users (
 
 CREATE SEQUENCE seq_users_pk
     START WITH 1 INCREMENT BY 1 NOCACHE;
+    
+/* BOARD */
+CREATE TABLE board (
+	no number PRIMARY KEY,
+    title varchar2(128) NOT NULL,
+    content varchar2(255) NOT NULL,
+    hit number DEFAULT 0,
+    reg_date date DEFAULT sysdate,
+    user_no number NOT NULL
+);
+
+CREATE SEQUENCE seq_board_pk
+    START WITH 1
+    INCREMENT BY 1;
+    
+SELECT * FROM board;
+
+--  게시물 리스트 쿼리
+SELECT
+    b.no,
+    title,
+    content,
+    hit,
+    reg_date as regDate,
+    user_no as userNo,
+    name as userName
+FROM board b, users u
+WHERE b.user_no = u.no;
+
+--  게시물 조회 쿼리
+SELECT
+    no,
+    title,
+    content
+FROM board;
